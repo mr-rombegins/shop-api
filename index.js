b17262f764cff6d5a900d83100caecb71c7fa9bf
Victor Kodzo Seayrom <mrseayrom@gmail.com>
2022-04-21 13:47:13 +0000

    Add authentication to API

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const { dbConnect } = require("./config/dbConnect");
+const authRouter = require("./routes/auth.router");
 const productRouter = require("./routes/products.router");
 
 const app = express();
 app.use(express.json());
 
+app.use("/auth", authRouter);
 app.use("/products", productRouter);
 
 const start = async () => {
